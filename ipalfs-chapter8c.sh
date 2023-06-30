9c2f9451392a6438ad1258cde852f68944687adc
Ivo van Poorten <ivopvp@gmail.com>
2023-06-30 03:01:30 +0200

    new python with everything we can compile with the libraries currently available

--- ipalfs-chapter8c.sh
+++ ipalfs-chapter8c.sh
@@ -17,6 +17,22 @@ export LDFLAGS=-static
 
 #if false; then
 
+# PYTHON
+
+cd $BUILD_PACKAGES
+tar xvJf $PACKAGES/Python-3.11.2.tar.xz
+cd Python-3.11.2
+./configure --prefix=/usr        \
+            --disable-shared     \
+            --enable-static      \
+            --with-system-expat  \
+            --with-system-ffi    \
+            --enable-optimizations
+cp $HOME/python-setup2.txt Modules/Setup
+make LINKFORSHARED=" " CFLAGS=-DOPENSSL_THREADS $PARALLEL
+make install
+strip /usr/bin/python3.11
+
 #fi
 
 # CLEANUP
